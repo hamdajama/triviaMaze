9c6e3c0695c64940d4c777e747226960588c2564
Eric John <ejohn5@uw.edu>
2024-08-01 14:05:21 -0700

    Small changes and moved my jar files around

--- src/model/QuestionGenerator.java
+++ src/model/QuestionGenerator.java
@@ -7,6 +7,8 @@ import java.sql.Statement;
 import java.util.ArrayList;
 import java.util.Collection;
 import java.util.List;
+import java.util.HashMap;
+import java.util.Map;
 import java.util.Random;
 /**
  * The QuestionGenerator class is responsible for generating random questions
