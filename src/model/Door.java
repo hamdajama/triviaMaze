cdafc5c256a780fc9536a1224e92f210682b689c
hamdajama <hamda.jama2525@gmail.com>
2024-08-09 21:46:35 -0700

    Update Door.java

--- src/model/Door.java
+++ src/model/Door.java
@@ -1,47 +1,57 @@
-/**
- * TCSS 360 - Trivia Maze
- * Door.java
- */
+
 package model;
+import model.DoorState;
 
 import java.io.Serializable;
-
 /**
  * The Door class represents a door in a room of the trivia maze.
- * A door can be either open or closed.
+ * A door can be either open or closed. Once a door is closed due to
+ * an incorrect answer, it remains permanently closed. The door is also
+ * associated with a direction.
  * @author hamda jama
  * @version 7/21/2024
  */
 public class Door implements Serializable {
     private static final long serialVersionUID = 4L;
     /**
-     * Indicates whether the door is closed.
+     * The direction of the door (e.g., NORTH, SOUTH, EAST, WEST).
      */
-    private boolean myClosed;
+    private final Direction myDirection;
+    /**
+     * The current state of the door (e.g., OPEN, CLOSED).
+     */
+    private DoorState myState;
+
     /**
      * Constructs a new Door, initially closed.
      */
-    public Door() {
-        this.myClosed = true;
+    public Door(Direction theDirection) {
+        this.myDirection = theDirection;
+        this.myState = DoorState.OPEN; // Doors are initally open.
+
     }
+    public Direction getDirection() {
+        return myDirection;
+    }
+
     /**
      * Checks if the door is closed.
      *
      * @return True if the door is closed, false otherwise.
      */
     public boolean isClosed() {
-        return myClosed;
+        return myState == DoorState.CLOSED;
     }
     /**
      * Opens the door.
      */
     public void open() {
-        myClosed = false;
+        myState = DoorState.OPEN;
     }
     /**
-     * Closes the door.
+     * Closes the door permanetly .
      */
     public void close() {
-        myClosed = true;
+        myState = DoorState.CLOSED;
     }
 }
